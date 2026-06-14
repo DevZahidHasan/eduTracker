c6c0408431118e9af3741943e0df13b2b155b7a2
zahidHasanExt <zahid.hasan.ext@holcim.com>
2026-06-15 01:40:34 +0600

    chore: completely remove backend scripts folder and sensitive data from version control on v1.0

--- backend/scripts/check-license.ts
+++
@@ -1,6 +0,0 @@
-import prisma from '../src/prisma';
-async function main() {
-  const s = await prisma.systemSetting.findUnique({ where: { key: 'LICENSE_KEY' } });
-  console.log('LICENSE_KEY:', s);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
